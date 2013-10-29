4a448918b4c353ca0634ee7846f1ef6479eff0ff
solome <sunshine1914@live.cn>
2013-10-29 17:07:36 +0800

    update code

---
+++ linux/shell/ilExist.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+# PROGRAM:judge the file is exist or not.
+# DATE:12:09:48 2013-10-29
+
+
+
+
---
+++ linux/shell/md.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+#作者：掬一捧 ｜solome@outlook.com ｜http://dolphinux.iliyang.cn/
+#功能：快捷進行markdown文檔的創建編輯。
+#說明：輸入文件名後，會自動在其後添加markdown文件的創建日期和後綴名.md。如果文件已經存在，則直接進入進行修改操作。
+#日期：2013/05/20
+
+
+
